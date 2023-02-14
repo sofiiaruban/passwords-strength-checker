92beb1b759e9e0f35f2ab49d6bb3dd277a5da626
Sofia Ruban <sophiaruban249324@gmail.com>
2023-02-14 22:01:31 +0200

    refactor: Replace password-strength component to the app component

--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl} from '@angular/forms';
 
 @Component({
@@ -8,47 +8,8 @@ import { FormControl} from '@angular/forms';
 })
 export class PasswordInputComponent {
   
-  password = new FormControl('');
-  red: string = "red";
-  yellow: string ="yellow";
-  green: string  = "green";
-  grey:string = "grey";
-  value: any;
-  length: number = 0;
-  first:string = this.grey;
-  second:string = this.grey;
-  third: string = this.grey;
-
-  ngOnInit() {
-    this.password.valueChanges.subscribe((value = '')=> {
-      const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
-      const letters = /[a-zA-Z]+/.test(value || "");
-      const numbers = /[0-9]+/.test(value as string);
-      const symbols = regex.test(value as string);
-
-      this.first = this.grey;
-      this.second = this.grey;
-      this.third = this.grey;
-
-      if (value && value.length > 0 && value.length < 8) {
-        this.first=this.red;
-        this.second=this.red;
-        this.third=this.red;
-      } 
-      if(value && value.length >= 8) {
-        this.first=this.red;
-      }
-
-      if(value && value.length >= 8 && (letters && numbers || numbers && symbols || symbols && letters)) {
-        this.first=this.yellow;
-        this.second=this.yellow;
-      }
-
-      if(value && value.length >= 8 && letters && numbers && symbols) {
-        this.first=this.green;
-        this.second=this.green;
-        this.third=this.green;
-      }
-    })
-  }
+  @Output() password: EventEmitter<string> = new EventEmitter<string>();
+   setValue(input: string | undefined){
+      this.password.emit(input);
+   }
 }
--- src/app/password-strength-section/password-strength-section.component.ts
+++ src/app/password-strength-section/password-strength-section.component.ts
@@ -7,13 +7,47 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class PasswordStrengthSectionComponent implements OnInit{
-  
-  @Input()
-  first!: string;
-  @Input()
-  second!: string;
-  @Input()
-  third!: string;
-  ngOnInit(): void {
+  red: string = "red";
+  yellow: string ="yellow";
+  green: string  = "green";
+  grey:string = "grey";
+  value: any;
+  length: number = 0;
+  first:string = this.grey;
+  second:string = this.grey;
+  third: string = this.grey;
+
+  ngOnInit() {
+    
+    this.password.valueChanges.subscribe((value = '')=> {
+      const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
+      const letters = /[a-zA-Z]+/.test(value || "");
+      const numbers = /[0-9]+/.test(value as string);
+      const symbols = regex.test(value as string);
+
+      this.first = this.grey;
+      this.second = this.grey;
+      this.third = this.grey;
+
+      if (value && value.length > 0 && value.length < 8) {
+        this.first=this.red;
+        this.second=this.red;
+        this.third=this.red;
+      } 
+      if(value && value.length >= 8) {
+        this.first=this.red;
+      }
+
+      if(value && value.length >= 8 && (letters && numbers || numbers && symbols || symbols && letters)) {
+        this.first=this.yellow;
+        this.second=this.yellow;
+      }
+
+      if(value && value.length >= 8 && letters && numbers && symbols) {
+        this.first=this.green;
+        this.second=this.green;
+        this.third=this.green;
+      }
+    })
   }
 }
