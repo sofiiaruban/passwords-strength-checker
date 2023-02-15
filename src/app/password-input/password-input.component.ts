4c354a0d79916f3af44c674e9bec78f3576651fb
Sofia Ruban <sophiaruban249324@gmail.com>
2023-02-15 14:01:37 +0200

    refactor: Add ControlValueAccessor to the password input component

--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,15 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl} from '@angular/forms';
+import { AfterViewInit, Component, EventEmitter, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
+import { ControlValueAccessor, DefaultValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-password-input',
   templateUrl: './password-input.component.html',
-  styleUrls: ['./password-input.component.scss']
+  styleUrls: ['./password-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: PasswordInputComponent,
+    multi: true
+  }]
 })
-export class PasswordInputComponent {
-  
-  @Output() password: EventEmitter<string> = new EventEmitter<string>();
-   setValue(input: string | undefined){
-      this.password.emit(input);
-   }
+export class PasswordInputComponent implements ControlValueAccessor, AfterViewInit {
+  @ViewChild(DefaultValueAccessor) private valueAccessor: DefaultValueAccessor | undefined;
+
+  private formControlChanged: any;
+  private formControlTouched: any;
+
+  public ngAfterViewInit(): void {
+    if (this.valueAccessor){
+      this.valueAccessor.registerOnChange(this.formControlChanged);
+      this.valueAccessor.registerOnTouched(this.formControlTouched);
+    }
+  }
+
+  public registerOnChange(fn: any): void {
+    this.formControlChanged = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.formControlTouched = fn;
+  }
+
+  public writeValue(value: string): void {
+    this.valueAccessor && this.valueAccessor.writeValue(value);
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.valueAccessor && this.valueAccessor.setDisabledState(isDisabled);
+  }
 }
--- src/app/password-strength-section/password-strength-section.component.ts
+++ src/app/password-strength-section/password-strength-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-password-strength-section',
@@ -6,48 +6,57 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./password-strength-section.component.scss']
 })
 
-export class PasswordStrengthSectionComponent implements OnInit{
+export class PasswordStrengthSectionComponent implements OnChanges {
   red: string = "red";
   yellow: string ="yellow";
   green: string  = "green";
   grey:string = "grey";
-  value: any;
   length: number = 0;
   first:string = this.grey;
   second:string = this.grey;
   third: string = this.grey;
+  sectionList: Array<string> = ["first", "second", "third"]
 
-  ngOnInit() {
+  @Input()
+  password!: string | null;
     
-    this.password.valueChanges.subscribe((value = '')=> {
-      const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
-      const letters = /[a-zA-Z]+/.test(value || "");
-      const numbers = /[0-9]+/.test(value as string);
-      const symbols = regex.test(value as string);
+  ngOnChanges() {
+     const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
+     const letters = /[a-zA-Z]+/.test(this.password || "");
+     const numbers = /[0-9]+/.test(this.password||'');
+     const symbols = regex.test(this.password||'');
 
-      this.first = this.grey;
-      this.second = this.grey;
-      this.third = this.grey;
+     //this.first = this.grey;
+     //this.second = this.grey;
+     //this.third = this.grey;
+     this.getColors(3,this.grey);
 
-      if (value && value.length > 0 && value.length < 8) {
-        this.first=this.red;
-        this.second=this.red;
-        this.third=this.red;
-      } 
-      if(value && value.length >= 8) {
-        this.first=this.red;
-      }
+     if (this.password && this.password.length > 0 && this.password.length < 8) {
+      this.getColors(3, this.red);
+       //this.first=this.red;
+       //this.second=this.red;
+       //this.third=this.red;
+     } 
+     if(this.password && this.password.length >= 8) {
+       this.first=this.red;
+       this.getColors(3,this.grey);
+     }
 
-      if(value && value.length >= 8 && (letters && numbers || numbers && symbols || symbols && letters)) {
-        this.first=this.yellow;
-        this.second=this.yellow;
-      }
+     if(this.password && this.password.length >= 8 && (letters && numbers || numbers && symbols || symbols && letters)) {
+       this.first=this.yellow;
+       this.second=this.yellow;
+     }
 
-      if(value && value.length >= 8 && letters && numbers && symbols) {
-        this.first=this.green;
-        this.second=this.green;
-        this.third=this.green;
-      }
-    })
+     if(this.password && this.password.length >= 8 && letters && numbers && symbols) {
+       this.first=this.green;
+       this.second=this.green;
+       this.third=this.green;
+     }
+  }
+
+  private getColors(counter:number, color:string) {
+    for (let i = 0; i < counter; i++){
+      (this as any)[this.sectionList[i]] = color;
+    }
   }
 }
